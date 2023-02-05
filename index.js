55734f29d07e0edac66f1175ef5d5da59d8f3d22
LonnyLaizer <124498406+LonnyLaizer@users.noreply.github.com>
2023-02-05 03:45:36 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+console.log("I like pizza");
+console.log("Its really goog!");
+
+window.alert("I REAL LOVE PIZZA");
+let age=22;
+let firstname="deus";
+let student="true";
+
+console.log("age");
+console.log("firstname");
+console.log("student");
+
+
